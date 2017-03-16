3fcef1c6c29fa3e70873395634e16aed5068110f
Jaagup Kippar <jaagup@tlu.ee>
2017-03-16 09:04:13 +0200

    Create EsimeneTest.java

---
+++ src/test/java/jaagup/EsimeneTest.java
@@ -0,0 +1,18 @@
+package jaagup;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class EsimeneTest{
+	@Test
+	public void katse1(){
+		assertEquals(3, 2+1);
+	}
+
+    @Test
+   public void tervituseTest(){
+		Rakendus r=new Rakendus();
+		assertEquals("Ahoi", r.tervitusfunktsioon());
+	}	
+
+}
